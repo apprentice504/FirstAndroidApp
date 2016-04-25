b8258046df4a6d3a834b165e8aab44b1d02cf98b
shoaib.khan <khan.1@campus.tu-berlin.de>
2016-04-25 15:43:15 +0200

    Added fragments, how to add and remove fragments from the dynamically from code behind

--- app/src/main/java/com/apprentice/firstandroidapp/MyActivity.java
+++ app/src/main/java/com/apprentice/firstandroidapp/MyActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
+import layout.BlueFragment;
+import layout.RedFragment;
+
 public class MyActivity extends AppCompatActivity {
-public final static String EXTRA_MESSAGE = "com.apprentice.firstandroidapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.apprentice.firstandroidapp.MESSAGE";
+    public Bundle savedInstanceState;
+    public boolean isBlueFragmentAdded;
+    Fragment newFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
         setContentView(R.layout.activity_my);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,17 +36,73 @@ public final static String EXTRA_MESSAGE = "com.apprentice.firstandroidapp.MESSA
                         .setAction("Action", null).show();
             }
         });
+
+
     }
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view){
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
         startActivity(intent);
     }
 
+    public void addFragment(View view) {
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            newFragment = new BlueFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+            isBlueFragmentAdded = true;
+            newFragment = new RedFragment();
+        }
+    }
+
+    public void replaceFragment(View view) {
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            if (isBlueFragmentAdded) {
+                newFragment = new RedFragment();
+                isBlueFragmentAdded = false;
+            } else {
+                newFragment = new BlueFragment();
+                isBlueFragmentAdded = true;
+            }
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
+
+    public void removeFragment(View view) {
+        if (findViewById(R.id.fragment_container) != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().remove(newFragment);
+            transaction.commit();
+        }
+    }
+
     @Override
-    public  void onPause(){
+    public void onPause() {
         super.onPause();
 
     }
